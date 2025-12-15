5f3291d213d879715c2a945eb3a905cb4a8d50ed
skamidi55 <kspoorthi.1029@gmail.com>
2025-12-14 22:45:19 -0500

    Added custom exceptions

--- src/main/java/com/example/accounts/dto/ResponseDto.java
+++ src/main/java/com/example/accounts/dto/ResponseDto.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDto {
     private String statusCode;
     private String statusMsg;
